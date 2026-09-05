c14f826d60cdf6eba09ef2c53eacf55c06ce3688
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2026-09-05 21:39:22 +0400

    chore(basic-server): drop redundant resolver return-type annotations
    @nestbridge/core now derives resolver return types from the @Query()/@Mutation()
    type thunk, so the explicit Promise<UserType> annotations are no longer needed.

--- examples/basic/server/src/users/users.resolver.ts
+++ examples/basic/server/src/users/users.resolver.ts
@@ -8,12 +8,12 @@ export class UsersResolver {
   constructor(@Inject(UsersService) private readonly usersService: UsersService) {}
 
   @Query(() => UserType)
-  findOne(@Args('id', { type: () => String }) id: string): Promise<UserType> {
+  findOne(@Args('id', { type: () => String }) id: string) {
     return this.usersService.findOne(id, true);
   }
 
   @Mutation(() => UserType, { name: 'createUser' })
-  create(@Args('name', { type: () => String }) name: string): Promise<UserType> {
+  create(@Args('name', { type: () => String }) name: string) {
     return this.usersService.create({ name });
   }
 }
